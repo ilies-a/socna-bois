59744a1cdf85d36af3bc6fd6ccb79dced5b7b8fd
ilies-a <111709356+ilies-a@users.noreply.github.com>
2024-12-07 00:23:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ run = st.button("Start Camera")
 
 if run:
     # Try different camera indices
-    cap = cv2.VideoCapture(i)
+    cap = cv2.VideoCapture(0)
     if cap.isOpened():
         stframe = st.empty()
         while cap.isOpened():
